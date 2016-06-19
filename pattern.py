ca53d1a78a50ce6a0214e8a9476b70e4a73f24ea
Lin <Brian@LinTzu-Hengs-MacBook-Air.local>
2016-06-19 22:24:44 +0800

    initial commit

---
+++ pattern.py
@@ -0,0 +1,11 @@
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import BernoulliRBM
+from sklearn.grid_search import GridSearchCV
+from sklearn.pipeline import Pipeline
+import numpy as np
+import argparse
+import time
+import cv2
+
